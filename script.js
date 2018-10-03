a92f16934d9ed3dd3b5642c01c79451ae4c1fc30
Luci-Lawless <lu.gmmm@gmail.com>
2018-10-03 12:09:23 +0200

    Integrate navbar and courses page (Erin)

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-// Import navbar
-// $(document).ready(function() {
-//   $('#navigation').load('nav.html');
-// });
+// Gallery text box effect
 $(document).ready(function() {
-  $('#fade-box').click(function(){ // Gallery text box effect
+  $('#fade-box').click(function(){
     $('p').fadeOut();
   })
   $('#gallery-title').click(function() {
     $('p').fadeIn();
   })
+});
+
+// Courses Page
+$(document).ready(function(){
+   $('.more-details-button').on('click', event => {
+    $(event.currentTarget).closest('.course-details').next().toggle()
+  });
+  $('.details').on('click', event => {
+    $(event.currentTarget).addClass('active');
+
+    $(event.currentTarget).siblings().removeClass('active')
+  });
 });
