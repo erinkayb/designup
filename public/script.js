96f80b65cbcb193d3568f401b2408f94c847d7ff
Erin de Blaeij <erinkayb@gmail.com>
2018-10-11 15:41:41 +0200

    localhost assignment

---
+++ public/script.js
@@ -0,0 +1,21 @@
+// Gallery text box effect
+$(document).ready(function() {
+  $('#fade-box').click(function(){
+    $('p').fadeOut();
+  })
+  $('#gallery-title').click(function() {
+    $('p').fadeIn();
+  })
+});
+
+// Courses
+$(document).ready(function(){
+   $('.more-details-button').on('click', event => {
+    $(event.currentTarget).closest('.course-details').next().toggle()
+  });
+  $('.details').on('click', event => {
+    $(event.currentTarget).addClass('active');
+
+    $(event.currentTarget).siblings().removeClass('active')
+  });
+});
